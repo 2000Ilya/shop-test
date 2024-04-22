43b3412081746cb70fa50c27cadc8471a0ebb585
2000Ilya <konick.ilya@yandex.ru>
2024-04-22 12:27:05 +0500

    deploy config added

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': " https://dev-su.eda1.ru/test_task",
-    }
-  }
+  base: "/shop-test/"
 })
